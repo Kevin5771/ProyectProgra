99f18c6e86d53bf881ef4c5bedbea8eb3519673b
Kevin5771 <kevin577gutierrez@gmail.com>
2024-10-16 20:55:12 -0600

    MainActivity

--- app/src/main/java/com/example/proyectoprogra/Chat/MainActivity2.kt
+++ app/src/main/java/com/example/proyectoprogra/Chat/MainActivity2.kt
@@ -1,4 +1,4 @@
-package com.example.proyectoprogra.Citas
+package com.example.proyectoprogra.Chat
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,17 +7,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.proyectoprogra.R
 
-class CreateAppoiment : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_create_appoiment)
+        setContentView(R.layout.activity_main3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
-            
         }
     }
 }
