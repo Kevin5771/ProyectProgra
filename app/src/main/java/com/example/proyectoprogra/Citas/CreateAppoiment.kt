731b3d83bb9f72d6010941009d4621fe753910c6
jU1fr <jlechugao@miumg.edu.gt>
2024-10-17 00:09:02 -0600

    Agregar Opciones en los Spinner

--- app/src/main/java/com/example/proyectoprogra/Citas/CreateAppoiment.kt
+++ app/src/main/java/com/example/proyectoprogra/Citas/CreateAppoiment.kt
@@ -2,7 +2,9 @@ package com.example.proyectoprogra.Citas
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -40,5 +42,16 @@ class CreateAppoiment : AppCompatActivity() {
             Toast.makeText(applicationContext, "Cita Realizada Exitosamente", Toast.LENGTH_SHORT).show()
             finish()
         }
+
+        val spinnerSpecialties = findViewById<Spinner>(R.id.spinner_especialidades)
+        val spinnerDoctor = findViewById<Spinner>(R.id.spinner_medico)
+
+
+        val optionSpecialtiels = arrayOf("Especialidad 1", "Especialidad 2", "Especialidad 3")
+        spinnerSpecialties.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, optionSpecialtiels)
+
+
+        val optionsDoctor = arrayOf("Medico 1", "Medico 2", "Medico 3")
+        spinnerDoctor.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, optionsDoctor)
     }
 }
