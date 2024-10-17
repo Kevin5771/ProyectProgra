b6be23f9501b861569605820bfe9773ed730d87f
jU1fr <jlechugao@miumg.edu.gt>
2024-10-16 22:32:39 -0600

    menu

--- app/src/main/java/com/example/proyectoprogra/Citas/Menu.kt
+++ app/src/main/java/com/example/proyectoprogra/Citas/Menu.kt
@@ -1,6 +1,8 @@
 package com.example.proyectoprogra.Citas
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +19,17 @@ class Menu : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnReservarCita = findViewById<Button>(R.id.btn_reservar_cita)
+        btnReservarCita.setOnClickListener {
+
+            goToCreateAppoiment()
+        }
+
+    }
+
+    private fun goToCreateAppoiment(){
+        val i = Intent(this, CreateAppoiment::class.java)
+        startActivity(i)
     }
 }
